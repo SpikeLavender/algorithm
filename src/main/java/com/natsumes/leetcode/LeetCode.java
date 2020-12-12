db55a6c3884df9894cb7e08259db2ce5ce50ae1d
hetengjiao <hetengjiao@chinamobile.com>
2020-12-13 05:48:33 +0800

    add some tree search
    Signed-off-by: hetengjiao <hetengjiao_xjtu@163.com>

--- src/main/java/com/natsumes/leetcode/LeetCode.java
+++ src/main/java/com/natsumes/leetcode/LeetCode.java
@@ -153,7 +153,7 @@ public class LeetCode {
 
 
     /**
-     * LC 二叉树的前序遍历
+     * LC 二叉树的前序遍历 || 144. 二叉树的前序遍历
      * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
      *
      * 示例 1：
@@ -240,7 +240,7 @@ public class LeetCode {
     }
 
     /**
-     * LC 二叉树的中序遍历
+     * LC 二叉树的中序遍历 || 94. 二叉树的中序遍历
      * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
      *
      *  
@@ -329,7 +329,9 @@ public class LeetCode {
     }
 
     /**
-     * LC 二叉树的后序遍历 给定一个二叉树，返回它的 后序 遍历。
+     * LC 二叉树的后序遍历  || 145 二叉树的后序遍历
+     *
+     * 给定一个二叉树，返回它的 后序 遍历。
      * <p>
      * 示例:
      *
@@ -428,7 +430,7 @@ public class LeetCode {
     }
 
     /**
-     * LC 二叉树的层序遍历
+     * LC 二叉树的层序遍历 || 102 二叉树的层序遍历
      * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
      *
      *
@@ -512,7 +514,7 @@ public class LeetCode {
     private int answer = 0;
 
     /**
-     * LC 二叉树的最大深度
+     * LC 二叉树的最大深度 || 104. 二叉树的最大深度
      * <p>
      * 给定一个二叉树，找出其最大深度。
      * <p>
@@ -538,6 +540,7 @@ public class LeetCode {
     public int maxDepth(TreeNode root) {
         //maxDepthTop(root, 1);
         //return answer;
+        answer = 0;
         return maxDepthBottom(root);
     }
 
@@ -563,7 +566,7 @@ public class LeetCode {
     }
 
     /**
-     * LC 对称二叉树
+     * LC 对称二叉树 || 剑指 Offer 28. 对称的二叉树
      * <p>
      * 给定一个二叉树，检查它是否是镜像对称的。
      * <p>
@@ -649,6 +652,208 @@ public class LeetCode {
         }
 
         return true;
+    }
+
+    /**
+     * 是否存在根节点到叶子节点的路径
+     */
+    private boolean hasPathSum;
+
+    /**
+     * LC 路径总和  || 112. 路径总和
+     * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     *
+     * 示例: 
+     * 给定如下二叉树，以及目标和 sum = 22，
+     *
+     *               5
+     *              / \
+     *             4   8
+     *            /   / \
+     *           11  13  4
+     *          /  \      \
+     *         7    2      1
+     * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xo566j/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        hasPathSum = false;
+        hasPathSumTop(root, sum);
+        //hasPathSumBottom(root, sum);
+        return hasPathSum;
+    }
+
+    /**
+     * 递归 -- 自顶向下
+     */
+    private void hasPathSumTop(TreeNode root, int sum) {
+        // 如果已经有满足条件的，就不需要再遍历了
+        if (root == null || hasPathSum) {
+            return;
+        }
+
+        if (root.left == null && root.right == null && sum == root.val) {
+            hasPathSum = true;
+            return;
+        }
+        hasPathSumTop(root.left, sum - root.val);
+        hasPathSumTop(root.right, sum - root.val);
+    }
+
+    /**
+     * 递归 -- 自底向上
+     */
+    private void hasPathSumBottom(TreeNode root, int sum) {
+        if (root == null || hasPathSum) {
+            return;
+        }
+        sum -= root.val;
+        hasPathSumBottom(root.left, sum);
+        hasPathSumBottom(root.right, sum);
+        if (sum == 0) {
+            hasPathSum = true;
+        }
+    }
+
+    /**
+     * LC 填充每个节点的下一个右侧节点指针 || 116. 填充每个节点的下一个右侧节点指针
+     * <p>
+     * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+     *
+     * struct Node {
+     *   int val;
+     *   Node *left;
+     *   Node *right;
+     *   Node *next;
+     * }
+     * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+     *
+     * 初始状态下，所有 next 指针都被设置为 NULL。
+     *
+     *
+     * 进阶：
+     *
+     * 你只能使用常量级额外空间。
+     * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+     *
+     * 输入：root = [1,2,3,4,5,6,7]
+     * 输出：[1,#,2,3,#,4,5,6,7,#]
+     *
+     * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+     * 序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+     *  
+     *
+     * 提示：
+     *
+     * 树中节点的数量少于 4096
+     * -1000 <= node.val <= 1000
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        connect(root.left, root.right);
+        return root;
+    }
+
+    /**
+     * 递归
+     */
+    private void connect(Node left, Node right) {
+
+        if (left == null || right == null) {
+            return;
+        }
+        left.next = right;
+        connect(left.left, left.right);
+        connect(right.left, right.right);
+        connect(left.right, right.left);
+    }
+
+    /**
+     * LC 填充每个节点的下一个右侧节点指针II || 117. 填充每个节点的下一个右侧节点指针II
+     *
+     * 给定一个二叉树
+     *
+     * struct Node {
+     *   int val;
+     *   Node *left;
+     *   Node *right;
+     *   Node *next;
+     * }
+     * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+     *
+     * 初始状态下，所有 next 指针都被设置为 NULL。
+     *
+     *  
+     *
+     * 进阶：
+     *
+     * 你只能使用常量级额外空间。
+     * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+     *  
+     *
+     * 示例：
+     *
+     *
+     *
+     * 输入：root = [1,2,3,4,5,null,7]
+     * 输出：[1,#,2,3,#,4,5,7,#]
+     * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+     *  
+     *
+     * 提示：
+     *
+     * 树中的节点数小于 6000
+     * -100 <= node.val <= 100
+     *  
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xorvud/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public Node connect2(Node root) {
+        return connect4Iteration(root);
+    }
+
+    /**
+     * 方法一：层序遍历，然后将每一层连起来成一个链表
+     */
+    private Node connect4Iteration(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 0; i < n; i++) {
+                Node f = queue.poll();
+                if (Objects.requireNonNull(f).left != null) {
+                    queue.offer(f.left);
+                }
+                if (f.right != null) {
+                    queue.offer(f.right);
+                }
+                if (i != 0) {
+                    last.next = f;
+                }
+                last = f;
+            }
+        }
+        return node;
     }
 
     /**
@@ -831,53 +1036,12 @@ public class LeetCode {
         p.right = right;
     }
 
-    /**
-     * 116. 填充每个节点的下一个右侧节点指针
-     * <p>
-     * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
-     *
-     * struct Node {
-     *   int val;
-     *   Node *left;
-     *   Node *right;
-     *   Node *next;
-     * }
-     * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
-     *
-     * 初始状态下，所有 next 指针都被设置为 NULL。
-     *
-     *
-     * 进阶：
-     *
-     * 你只能使用常量级额外空间。
-     * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
-     *
-     * 输入：root = [1,2,3,4,5,6,7]
-     * 输出：[1,#,2,3,#,4,5,6,7,#]
-     *
-     * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
-     * 序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
-     *  
-     *
-     * 提示：
-     *
-     * 树中节点的数量少于 4096
-     * -1000 <= node.val <= 1000
-     *
-     * 来源：力扣（LeetCode）
-     * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
-     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-     */
-    public Node connect(Node root) {
-        if (root == null) {
-            return null;
-        }
-        return null;
-    }
+
 
     private int ans = Integer.MIN_VALUE;
     /**
-     * 124. 二叉树中的最大路径和 给定一个非空二叉树，返回其最大路径和。
+     * 124. 二叉树中的最大路径和
+     * 给定一个非空二叉树，返回其最大路径和。
      * <p>
      * 本题中，路径被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。 该路径至少包含一个节点，且不一定经过根节点。
      * <p>
--- src/test/java/com/natsumes/leetcode/LeetCodeTest.java
+++ src/test/java/com/natsumes/leetcode/LeetCodeTest.java
@@ -232,4 +232,58 @@ public class LeetCodeTest {
         System.out.println(i);
         Assert.assertFalse(i);
     }
+
+    /**
+     * 示例: 
+     * 给定如下二叉树，以及目标和 sum = 22，
+     *
+     *               5
+     *              / \
+     *             4   8
+     *            /   / \
+     *           11  13  4
+     *          /  \      \
+     *         7    2      1
+     * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+     */
+    @Test
+    public void hasPathSum() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11, new TreeNode(7), new TreeNode(2));
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4, null, new TreeNode(1));
+        boolean i = lt.hasPathSum(root, 22);
+        System.out.println(i);
+        Assert.assertTrue(i);
+
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+        i = lt.hasPathSum(root, 22);
+        System.out.println(i);
+        Assert.assertFalse(i);
+    }
+
+    @Test
+    public void connect() {
+        Node root = new Node(1);
+        root.left = new Node(2, new Node(4), new Node(5), null);
+        root.right = new Node(3, new Node(6), new Node(7), null);
+        Node node = lt.connect(root);
+        System.out.println(node);
+    }
+
+    @Test
+    public void connect2() {
+        Node root = new Node(1);
+        root.left = new Node(2, new Node(4), new Node(5), null);
+        root.right = new Node(3, null, new Node(7), null);
+        Node node = lt.connect2(root);
+        System.out.println(node);
+    }
 }
