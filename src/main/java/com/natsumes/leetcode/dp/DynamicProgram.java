29f9a119032b96756fae5722eac66a58aab8800d
hetengjiao <hetengjiao@chinamobile.com>
2021-02-03 16:40:58 +0800

    update test bug
    Signed-off-by: hetengjiao <hetengjiao_xjtu@163.com>

--- src/main/java/com/natsumes/leetcode/dp/DynamicProgram.java
+++ src/main/java/com/natsumes/leetcode/dp/DynamicProgram.java
@@ -1,7 +1,13 @@
 package com.natsumes.leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * 动态规划
+ *
+ * <a href="https://leetcode-cn.com/problems/longest-increasing-subsequence/">300.最长递增子序列</a>
+ * @see DynamicProgram#lengthOfLIS(int[])
+ * @see DynamicProgram#patienceSorting(int[])
  *
  * <a href="https://leetcode-cn.com/problems/maximum-subarray/">53.最大子序和</a>
  * @see DynamicProgram#maxSubArray(int[])
@@ -9,6 +15,84 @@ package com.natsumes.leetcode.dp;
  * @author hetengjiao
  */
 public class DynamicProgram {
+
+    /**
+     * 300. 最长递增子序列
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     *
+     * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+     * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [10,9,2,5,3,7,101,18]
+     * 输出：4
+     * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+     * 示例 2：
+     *
+     * 输入：nums = [0,1,0,3,2,3]
+     * 输出：4
+     * 示例 3：
+     *
+     * 输入：nums = [7,7,7,7,7,7,7]
+     * 输出：1
+     *
+     * @param nums nums
+     * @return int
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        // dp[i] = max(dp[i], dp[j] + 1)
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        for (int value : dp) {
+            max = Math.max(max, value);
+        }
+        return max;
+    }
+
+    /**
+     * 耐心排序
+     *
+     * @param nums nums
+     * @return int
+     */
+    public int patienceSorting(int[] nums) {
+        int[] top = new int[nums.length];
+        int piles = 0;
+        for (int poker : nums) {
+            int left = 0;
+            int right = piles;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (top[mid] > poker) {
+                    right = mid;
+                } else if (top[mid] < poker) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            if (left == piles) {
+                piles++;
+            }
+            top[left] = poker;
+        }
+        return piles;
+    }
+
     /**
      * 53. 最大子序和
      * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
--- src/test/java/com/natsumes/leetcode/dp/DynamicProgramTest.java
+++ src/test/java/com/natsumes/leetcode/dp/DynamicProgramTest.java
@@ -18,4 +18,18 @@ public class DynamicProgramTest {
         Assert.assertEquals(-1, dp.maxSubArray(new int[] {-1}));
         Assert.assertEquals(-100000, dp.maxSubArray(new int[] {-100000}));
     }
+
+    @Test
+    public void lengthOfLIS() {
+        Assert.assertEquals(4, dp.lengthOfLIS(new int[] {10, 9, 2, 5, 3, 7, 101, 18}));
+        Assert.assertEquals(4, dp.lengthOfLIS(new int[] {0, 1, 0, 3, 2, 3}));
+        Assert.assertEquals(1, dp.lengthOfLIS(new int[] {7, 7, 7, 7, 7, 7, 7}));
+    }
+
+    @Test
+    public void patienceSorting() {
+        Assert.assertEquals(4, dp.patienceSorting(new int[] {10, 9, 2, 5, 3, 7, 101, 18}));
+        Assert.assertEquals(4, dp.patienceSorting(new int[] {0, 1, 0, 3, 2, 3}));
+        Assert.assertEquals(1, dp.patienceSorting(new int[] {7, 7, 7, 7, 7, 7, 7}));
+    }
 }
