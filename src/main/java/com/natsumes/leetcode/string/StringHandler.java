8ae1cf11d232961ad9c5aa30de79f559a94429b2
hetengjiao <hetengjiao@chinamobile.com>
2021-02-26 14:17:20 +0800

    12. 整数转罗马数字
    Signed-off-by: hetengjiao <hetengjiao_xjtu@163.com>

--- src/main/java/com/natsumes/leetcode/string/StringHandler.java
+++ src/main/java/com/natsumes/leetcode/string/StringHandler.java
@@ -618,7 +618,16 @@ public class StringHandler {
      * @return String
      */
     public String intToRoman(int num) {
-        return "";
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length && num >= 0; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
     }
 
     /**
@@ -683,23 +692,40 @@ public class StringHandler {
      * @return int
      */
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>(16);
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        int n = s.length();
+        s = s.replace("IV", "a");
+        s = s.replace("IX", "b");
+        s = s.replace("XL", "c");
+        s = s.replace("XC", "d");
+        s = s.replace("CD", "e");
+        s = s.replace("CM", "f");
 
         int sum = 0;
-        for (int i = n; i > 0; i--) {
-            sum  = sum + i;
+        for (int i = 0; i < s.length(); i++) {
+            sum  = sum + char2Int(s.charAt(i));
         }
-
         return sum;
     }
+
+    private int char2Int(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            case 'a': return 4;
+            case 'b': return 9;
+            case 'c': return 40;
+            case 'd': return 90;
+            case 'e': return 400;
+            case 'f': return 900;
+            default:
+                throw new IllegalStateException("Unexpected value: " + c);
+        }
+    }
+
 
     /**
      * 1178. 猜字谜
